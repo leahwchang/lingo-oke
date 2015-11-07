5521e01dc1ee36769b214a622869e61a49c12266
Leah W. Chang <leahwchang@gmail.com>
2015-11-07 17:51:13 -0500

    clean up js code for displaying karaoke texts

--- app/assets/javascripts/songtexts.js
+++ app/assets/javascripts/songtexts.js
@@ -1,162 +1,52 @@
 document.addEventListener( "DOMContentLoaded", function() {
+	// get json file below
+	// split string into array
 	$.getJSON( "/languages.json", function(json) {
 		var trans1 = json[0].songs[0].translations[0].varbody;
 		var split1 = trans1.split(",");
-	  // console.log( split1 );
+	  console.log( split1 );
 	  var trans2 = json[0].songs[0].translations[1].varbody;
 		var split2 = trans2.split(",");
-	  // console.log( split2 );
+	  console.log( split2 );
 	  var trans3 = json[0].songs[0].translations[2].varbody;
 		var split3 = trans3.split(",");
-	  // console.log( split3 );
+	  console.log( split3 );
+
+		// KARAOKE EFFECT: loop through the texts to get highlighted effect
+
+		// popcorn.js below, showing karaoke texts according to seconds
+		// FUTURE: looping through the start and end time, increment of 2
+		// and concatenate the target div and text index 
 		var popcorn = Popcorn( ".audioplayer" );
+		var popObj = [
+			// mandarin
+	    {target: "footnote1", text: split1[0], start: 8.5, end: 12 },
+	    {target: "footnote1", text: split1[1], start: 12.5, end: 16 },
+	    {target: "footnote1", text: split1[2], start: 16.5, end: 20 },
+	    {target: "footnote1", text: split1[3], start: 20.5, end: 24 },
+	    // pinyin
+	    {target: "footnote2", text: split2[0], start: 8.5, end: 12 },
+	    {target: "footnote2", text: split2[1], start: 12.5, end: 16 },
+	    {target: "footnote2", text: split2[2], start: 16.5, end: 20 },
+	    {target: "footnote2", text: split2[3], start: 20.5, end: 24 },
+	    // english
+	    {target: "footnote3", text: split3[0], start: 8.5, end: 12 },
+	    {target: "footnote3", text: split3[1], start: 12.5, end: 16 },
+	    {target: "footnote3", text: split3[2], start: 16.5, end: 20 },
+	    {target: "footnote3", text: split3[3], start: 20.5, end: 24 },
+		];
 
-		// FUTURE: looping through the start and end time, and concatenate the target div and text index 
-		// loop through the texts to get highlighted effect
-
-		popcorn.footnote({
-		start: 8.5,
-		end: 10,
-		target: "footnote1",
-		text: split1[0]
-		});
-		popcorn.footnote({
-		start: 10.5,
-		end: 12,
-		target: "footnote1",
-		text: split1[1]
-		});
-		popcorn.footnote({
-		start: 12.5,
-		end: 14,
-		target: "footnote1",
-		text: split1[2]
-		});
-		popcorn.footnote({
-		start: 14.5,
-		end: 16,
-		target: "footnote1",
-		text: split1[3]
-		});
-		popcorn.footnote({
-		start: 16.5,
-		end: 18,
-		target: "footnote1",
-		text: split1[4]
-		});
-		popcorn.footnote({
-		start: 18.5,
-		end: 20,
-		target: "footnote1",
-		text: split1[5]
-		});
-		popcorn.footnote({
-		start: 20.5,
-		end: 22,
-		target: "footnote1",
-		text: split1[6]
-		});
-		popcorn.footnote({
-		start: 22.5,
-		end: 24,
-		target: "footnote1",
-		text: split1[7]
-		});
-		popcorn.footnote({
-		start: 8.5,
-		end: 10,
-		target: "footnote2",
-		text: split2[0]
-		});
-		popcorn.footnote({
-		start: 10.5,
-		end: 12,
-		target: "footnote2",
-		text: split2[1]
-		});
-		popcorn.footnote({
-		start: 12.5,
-		end: 14,
-		target: "footnote2",
-		text: split2[2]
-		});
-		popcorn.footnote({
-		start: 14.5,
-		end: 16,
-		target: "footnote2",
-		text: split2[3]
-		});
-		popcorn.footnote({
-		start: 16.5,
-		end: 18,
-		target: "footnote2",
-		text: split2[4]
-		});
-		popcorn.footnote({
-		start: 18.5,
-		end: 20,
-		target: "footnote2",
-		text: split2[5]
-		});
-		popcorn.footnote({
-		start: 20.5,
-		end: 22,
-		target: "footnote2",
-		text: split2[6]
-		});
-		popcorn.footnote({
-		start: 22.5,
-		end: 24,
-		target: "footnote2",
-		text: split2[7]
-		});
-		popcorn.footnote({
-		start: 8.5,
-		end: 10,
-		target: "footnote3",
-		text: split3[0]
-		});
-		popcorn.footnote({
-		start: 10.5,
-		end: 12,
-		target: "footnote3",
-		text: split3[1]
-		});
-		popcorn.footnote({
-		start: 12.5,
-		end: 14,
-		target: "footnote3",
-		text: split3[2]
-		});
-		popcorn.footnote({
-		start: 14.5,
-		end: 16,
-		target: "footnote3",
-		text: split3[3]
-		});
-		popcorn.footnote({
-		start: 16.5,
-		end: 18,
-		target: "footnote3",
-		text: split3[4]
-		});
-		popcorn.footnote({
-		start: 18.5,
-		end: 20,
-		target: "footnote3",
-		text: split3[5]
-		});
-		popcorn.footnote({
-		start: 20.5,
-		end: 22,
-		target: "footnote3",
-		text: split3[6]
-		});
-		popcorn.footnote({
-		start: 22.5,
-		end: 24,
-		target: "footnote3",
-		text: split3[7]
+		// iterating through the array and call on the function
+		// function is the popcorn footnote plugin
+		$.each(popObj, function(key, note) {
+		  popcorn.footnote({
+	      start: note.start,
+	      end: note.end,
+	      text: note.text,
+	      target: note.target,
+	      effect: "applyclass",
+	      applyclass: "selected"
+		  });
 		});
 	}, false );
 });
