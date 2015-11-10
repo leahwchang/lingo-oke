87f7047dcaea83f44a754559c256e063a5e9f61d
Leah W. Chang <leahwchang@gmail.com>
2015-11-10 07:24:49 -0500

    play audio and texts at the same time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= popcorn-complete
 //= require turbolinks
 //= require_tree .
 
@@ -21,69 +20,3 @@ $(document).ready(function() {
 	$(".songs").slideToggle( "slow" );
 		});
 });
-
-// $.getJSON( "/languages.json", function(json) {
-// 	var trans1 = json[0].songs[0].translations[0].varbody;
-// 	var split1 = trans1.split(",");
-//   console.log( split1 );
-//   var trans2 = json[0].songs[0].translations[1].varbody;
-// 	var split2 = trans2.split(",");
-//   console.log( split2 );
-//   var trans3 = json[0].songs[0].translations[2].varbody;
-// 	var split3 = trans3.split(",");
-//   console.log( split3 );
-// });
-
-
-// $(document).ready(function() {
-// 		$.getJSON( "/languages.json", function(json) {
-// 			var trans1 = json[0].songs[0].translations[0].varbody;
-// 			var split1 = trans1.split(",");
-// 		  console.log( split1 );
-// 		  var trans2 = json[0].songs[0].translations[1].varbody;
-// 			var split2 = trans2.split(",");
-// 		  console.log( split2 );
-// 		  var trans3 = json[0].songs[0].translations[2].varbody;
-// 			var split3 = trans3.split(",");
-// 		  console.log( split3 );
-
-// 		  document.addEventListener( "DOMContentLoaded", function() {
-// 			var popcorn = Popcorn( ".audioplayer" );
-// 			popcorn.footnote({
-// 			start: 8.5,
-// 			end: 10,
-// 			target: "footnote",
-// 			text: trans1
-// 			});
-// 		}, false );
-// 		});
-// 	});
-
-// obj = JSON.parse("/languages.json");
-// document.getElementById(".footnote").innerHTML = obj.songs[0].translations[0].varbody;
-
-
-// 	document.addEventListener( "DOMContentLoaded", function() {
-// 		var popcorn = Popcorn( ".audioplayer" );
-// 		popcorn.footnote({
-// 		start: 8.5,
-// 		end: 10,
-// 		target: "#footnote",
-// 		text: split1,
-// 		});
-// 	},
-// 	false );
-// });
-
-// document.addEventListener( "DOMContentLoaded", function() {
-// 				var popcorn = Popcorn( ".audioplayer" );
-// 				// var translations = document.getElementById("translation_0").innerHTML;
-// 				var obj = JSON.parse(myJSONObject);
-// 				popcorn.footnote({
-// 				start: 8.5,
-// 				end: 10,
-// 				target: "footnote",
-// 				text: myJSONObject,
-// 				});
-
-
--- app/assets/javascripts/songtexts.js
+++ app/assets/javascripts/songtexts.js
@@ -1,52 +1,75 @@
-document.addEventListener( "DOMContentLoaded", function() {
-	// get json file below
-	// split string into array
-	$.getJSON( "/languages.json", function(json) {
-		var trans1 = json[0].songs[0].translations[0].varbody;
-		var split1 = trans1.split(",");
-	  console.log( split1 );
-	  var trans2 = json[0].songs[0].translations[1].varbody;
-		var split2 = trans2.split(",");
-	  console.log( split2 );
-	  var trans3 = json[0].songs[0].translations[2].varbody;
-		var split3 = trans3.split(",");
-	  console.log( split3 );
+// retrive json data and split by verses
+$.getJSON( "/languages.json", function(json) {
+    var trans1 = json[0].songs[0].translations[0].varbody;
+    var split1 = trans1.split(",");
+    console.log( split1 );
+    var trans2 = json[0].songs[0].translations[1].varbody;
+    var split2 = trans2.split(",");
+    console.log( split2 );
+    var trans3 = json[0].songs[0].translations[2].varbody;
+    var split3 = trans3.split(",");
+    console.log( split3 );
 
-		// KARAOKE EFFECT: loop through the texts to get highlighted effect
 
-		// popcorn.js below, showing karaoke texts according to seconds
-		// FUTURE: looping through the start and end time, increment of 2
-		// and concatenate the target div and text index 
-		var popcorn = Popcorn( ".audioplayer" );
-		var popObj = [
-			// mandarin
-	    {target: "footnote1", text: split1[0], start: 8.5, end: 12 },
-	    {target: "footnote1", text: split1[1], start: 12.5, end: 16 },
-	    {target: "footnote1", text: split1[2], start: 16.5, end: 20 },
-	    {target: "footnote1", text: split1[3], start: 20.5, end: 24 },
-	    // pinyin
-	    {target: "footnote2", text: split2[0], start: 8.5, end: 12 },
-	    {target: "footnote2", text: split2[1], start: 12.5, end: 16 },
-	    {target: "footnote2", text: split2[2], start: 16.5, end: 20 },
-	    {target: "footnote2", text: split2[3], start: 20.5, end: 24 },
-	    // english
-	    {target: "footnote3", text: split3[0], start: 8.5, end: 12 },
-	    {target: "footnote3", text: split3[1], start: 12.5, end: 16 },
-	    {target: "footnote3", text: split3[2], start: 16.5, end: 20 },
-	    {target: "footnote3", text: split3[3], start: 20.5, end: 24 },
-		];
+// click button #play and play verses and song at the same time
+  // play verses below
+	var songtext;
 
-		// iterating through the array and call on the function
-		// function is the popcorn footnote plugin
-		$.each(popObj, function(key, note) {
-		  popcorn.footnote({
-	      start: note.start,
-	      end: note.end,
-	      text: note.text,
-	      target: note.target,
-	      effect: "applyclass",
-	      applyclass: "selected"
-		  });
-		});
-	}, false );
+	$('#play').click(function() {
+    var popcorn = Popcorn( "#audioplayer" );
+    var popObj = [
+      // mandarin
+      {target: "footnote1", text: split1[0], start: 8.5, end: 12 },
+      {target: "footnote1", text: split1[1], start: 12.5, end: 16 },
+      {target: "footnote1", text: split1[2], start: 16.5, end: 20 },
+      {target: "footnote1", text: split1[3], start: 20.5, end: 24 },
+      // pinyin
+      {target: "footnote2", text: split2[0], start: 8.5, end: 12 },
+      {target: "footnote2", text: split2[1], start: 12.5, end: 16 },
+      {target: "footnote2", text: split2[2], start: 16.5, end: 20 },
+      {target: "footnote2", text: split2[3], start: 20.5, end: 24 },
+      // english
+      {target: "footnote3", text: split3[0], start: 8.5, end: 12 },
+      {target: "footnote3", text: split3[1], start: 12.5, end: 16 },
+      {target: "footnote3", text: split3[2], start: 16.5, end: 20 },
+      {target: "footnote3", text: split3[3], start: 20.5, end: 24 },
+    ];
+
+  $.each(popObj, function(key, note) {
+    popcorn.footnote({
+      start: note.start,
+      end: note.end,
+      text: note.text,
+      target: note.target
+      // effect: "applyclass",
+      // applyclass: "selected"
+    });
+  });
+
+// play new colored texts below
+
+  // var cc = 0;
+  // function nextWord() {
+  //   $('#p'+cc).css("color", "red");
+  //   cc++;
+  //   if(cc> songtext.length-1) return;
+  //   window.setTimeout(nextWord, songtext[cc-1][1]);
+  // }
+
+  // play audio below
+  var audioElement = document.getElementById("audioplayer");
+    
+    audioElement.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    
+    $('#play').click(function() {
+        audioElement.play();
+    });
+    
+    $('#pause').click(function() {
+        audioElement.pause();
+    });
+	});
 });
