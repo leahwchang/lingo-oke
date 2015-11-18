bf5eee7bf2d06f0a423a075054ab9e7c322bbc6b
Leah W. Chang <leahwchang@gmail.com>
2015-11-18 18:42:10 -0500

    add showing favorited songs on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+
   def show
+
   	@user = current_user
+  	@favorite_songs = @user.favorite_songs
   	# show lesson view tracking: completed lessons and incompleted lessons
   	# use view count
 
